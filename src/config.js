ce35c1da0293d87ce2a4caf939e7dc346e2bc037
stockerssld <35477074+stockerssld@users.noreply.github.com>
2020-05-03 19:54:36 -0500

    Helpers and config

---
+++ src/config.js
@@ -0,0 +1,16 @@
+const API_URL = 'https://api.themoviedb.org/3/'
+const API_KEY = '844dba0bfd8f3a4f3799f6130ef9e335'
+
+const IMAGE_BASE_URL ='http://image.tmdb.org/t/p/';
+
+const BACKDROP_SIZE = 'w1280'
+
+const POSTER_SIZE= 'w500'
+
+export {
+    API_KEY,
+    API_URL,
+    IMAGE_BASE_URL,
+    BACKDROP_SIZE,
+    POSTER_SIZE
+}
