6ffcb84833f81332c2874e460943d0157ae51e66
stockerssld <35477074+stockerssld@users.noreply.github.com>
2020-05-07 15:02:55 -0500

    index

---
+++ src/components/App/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
+import Header from '../elements/Header'
+import Home from '../Home'
+import Movie from '../Movie'
+import NotFound from '../elements/NotFoud'
+
+export const App=()=>(
+    <BrowserRouter>
+        <>
+        <Header/>
+        <Switch>
+            <Route path="/" component={Home} exact/>
+            <Route path="/:movieId" component={Movie} exact/>
+            <Route component={NotFound}/>
+        </Switch>
+        </>
+    </BrowserRouter>
+)
